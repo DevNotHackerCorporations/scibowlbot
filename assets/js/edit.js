1bbf0237348eb38f6bcbb0d564c0b859f9ccdf1c
actiniumn404 <83673744+actiniumn404@users.noreply.github.com>
2023-03-04 21:22:56 -0800

    Switch page bar

--- assets/js/edit.js
+++ assets/js/edit.js
@@ -13,9 +13,10 @@ async function fetch_questions(){
 
 function search(query){
     Results = []
+    query = query.toLowerCase()
     for (let subject in Questions){
         for (let question of Questions[subject]){
-            if (question.tossup_question.includes(query) || question.tossup_answer.includes(query) || question.id.toString() === query){
+            if (question.tossup_question.toLowerCase().includes(query) || question.tossup_answer.toLowerCase().includes(query) || question.id.toString() === query){
                 Results.push(question)
            }
         }
@@ -33,7 +34,7 @@ function search_and_display(query){
     search(query)
 
     let count = 0
-    while (count < 50){
+    while (count < results_per_page && count < Results.length){
         let question = Results[count]
         $("#results").append(`
         <div class="result">
@@ -43,6 +44,33 @@ function search_and_display(query){
             <span><b>ANSWER: </b> ${question.tossup_answer}</span>
         </div>`)
         count++
+    }
+
+    let amount = Math.ceil(Results.length / results_per_page)
+    if (amount < 1){
+        $("#results").append(`<div id="next_page"><div class="page_link highlighted" data-goto="0">1</div></div>`)
+    }else{
+        let res = `<div id="next_page">
+            <div class="page_link"><i class="fa fa-angle-left" id="next_page_PREV"></i></div>
+            <div class="page_link" data-goto="0">1</div>
+            <div class="page_link" data-goto="1">2</div>`
+
+            if (amount > 4){
+                res += `<div class="page_link" id="next_page_CUSTOM">...</div>`
+            }
+            if (amount === 3){
+                res += `<div class="page_link" data-goto="2">3</div>`
+            }
+            else if (amount === 4){
+                res += `<div class="page_link" data-goto="2">3</div>`
+                res += `<div class="page_link" data-goto="3">4</div>`
+            }else{
+                res += `<div class="page_link" data-goto="${amount - 2}">${amount - 1}</div>`
+                res += `<div class="page_link" data-goto="${amount - 1}">${amount}</div>`
+            }
+
+        res += `<div class="page_link"><i class="fa fa-angle-right" id="next_page_NEXT"></i></div></div>`
+        $("#results").append(res)
     }
 
     $("#search__statistics").text(`${Results.length} Result${Results.length === 1 ? "" : "s"} in ${((performance.now() - start)/1000).toFixed(3)} Seconds`)
